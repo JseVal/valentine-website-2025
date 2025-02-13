8e58f918976a9e8b333fe01a7d45f364cb84ee43
JseVal <jesse13valenciajr@gmail.com>
2025-02-12 18:55:55 -0600

    Update config.js

--- config.js
+++ config.js
@@ -42,15 +42,15 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "YAYYYYYYYYYY! 🚀💝",              // Shows when they go past 1000%
+        extreme: "WOOOOW You love me that much :) 🥰🚀💝",  // Shows when they go past 5000%
+        high: "YAYYYYYYYYYY! 💝",              // Shows when they go past 1000%
         normal: "HEHE! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Giving you a big warm hug and a huge kiss! I LOVE YOUUU",
+        title: "Yay! I'm the luckiest person in the world! 💝💖💝💓",
+        message: "I LOVE YOUUU SOOO MUCHH!!!!!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
